346260b73c8b5b80c545455e19aad7074ac5bcf1
郑永震 <717015340@qq.com>
2021-08-24 16:00:40 +0800

    third commit -- add two classes

---
+++ crawler/src/main/java/com/pa/pojo/GitProject.java
@@ -0,0 +1,11 @@
+package com.pa.pojo;
+
+/**
+ * @author 郑永震
+ * @date 2021/8/24 0024 15:59
+ * @mail zheng_yzh@163.com
+ */
+public class GitProject {
+    private int id;
+    private String name;
+}
---
+++ crawler/src/main/java/com/pa/utils/ParseUtil.java
@@ -0,0 +1,13 @@
+package com.pa.utils;
+
+/**
+ * @author 郑永震
+ * @date 2021/8/24 0024 15:59
+ * @mail zheng_yzh@163.com
+ */
+public class ParseUtil {
+
+    public static void parseXml(String path){
+
+    }
+}
